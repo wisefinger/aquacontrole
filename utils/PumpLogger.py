fa6eb108b1240b45c5960babfa0b6d0407b7c075
wisefinger <kris.cardoen@gmail.com>
2020-08-21 15:53:37 +0200

    fixed issue get latestpumplof file not founf error

--- utils/PumpLogger.py
+++ utils/PumpLogger.py
@@ -14,24 +14,41 @@ class PumpLogger:
                 f.write("\n")
     # append data at the end of the logfile
     def log(self,message):
-        with open('logging/pump.log', 'a') as f:
-            f.write(f'{datetime.now().strftime("%d/%m/%Y:%X")}: {message}')
-            f.write("\n")
+        
+        if path.exists('logging/pump.log'):
+            with open('logging/pump.log', 'a') as f:
+                f.write(f'{datetime.now().strftime("%d/%m/%Y:%X")}: {message}')
+                f.write("\n")
+                
+        else:
+            with open('logging/pump.log', 'a') as f:
+                f.write(f'{datetime.now().strftime("%d/%m/%Y:%X")}: new log created.')
+                f.write("\n")
+                
+                
 
     # get last the last rundate of the logfile
     def getlatest(self):
+        
+        if path.exists('logging/pump.log'):
 
-        with open('logging/pump.log', 'r') as f:
-            for line in f:
-                line = line.strip(' \t\r\n')
-                if line.__contains__(('stop')):
-                    last_line = line
-                pass
-        try:
-            if last_line.__contains__(('stop')):
-                return last_line
-            else:
-                return None
-        except UnboundLocalError:
-            print('Empty logfile no previous history')
+            with open('logging/pump.log', 'r') as f:
+                for line in f:
+                    line = line.strip(' \t\r\n')
+                    if line.__contains__(('stop')):
+                        last_line = line
+                    pass
+            try:
+                if last_line.__contains__(('stop')):
+                    return last_line
+                else:
+                    return None
+            except UnboundLocalError:
+                print('Empty logfile no previous history')
+        
+        else:
+            with open('logging/pump.log', 'a') as f:
+                f.write(f'{datetime.now().strftime("%d/%m/%Y:%X")}: new log created.')
+                f.write("\n")
             return None
+    
