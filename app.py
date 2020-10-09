428c4eb5795cfa395ac3bd577d635d54b10e573e
wisefinger <kris.cardoen@gmail.com>
2020-10-09 09:10:24 +0200

    change app.py to call temp url

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from utils.Pump import Pump
 from utils.TempSensor import TempSensor
 from utils.PumpLogger import PumpLogger
 from utils.ActionLogger import ActionLogger
+import requests
 import utils.archive_file
+
 
 print(f"{datetime.now()} start main aquacontrole module.....................")
 # Create an instance to log actions into the generic /logger/action.log
@@ -19,7 +21,7 @@ stopsignal = False
 # Create an instance for the pump controller
 pump = Pump()
 # Create and instance for the temp sensor 1
-tempsensor_1 = TempSensor()
+# tempsensor_1 = TempSensor()
 # Create an instance to log pump actions into the file /logger/pump.log
 pumplogger = PumpLogger()
 # start action loop
@@ -27,8 +29,14 @@ while not stopsignal:
     # execute actions
     print('> start controle loop ')
     # measure tempsensor 1
-    print(f'>> tempsensor 1 = {tempsensor_1.getTemp()}')
+    #print(f'>> tempsensor 1 = {tempsensor_1.getTemp()}')
     # archive all folders older then
+    r =requests.get("http://127.0.0.1:5000/datastream/1")
+    print(f'request :{r.request} ')
+    print(f'status code :{r.status_code}')
+    print(f'headers :{r.headers}')
+    print(f'body : {r.text}')
+    
     try:
         print("start archive from app.py")
         utils.archive_file.archive()
@@ -36,7 +44,7 @@ while not stopsignal:
     except FileExistsError:
         print("file could not be archived, archived file already exists")
     
-    #print('> end controle loop ')
+    print('> end controle loop ')
 
     # Check if the daily water change has already ran today. If not start it now
     # First check if a previous date was found
