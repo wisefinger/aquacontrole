668e235f9d7c2bdb4b7b379f9a374ee2bc90372e
wisefinger <kris.cardoen@gmail.com>
2020-11-19 22:22:31 +0100

    added json to html package

--- temp-class-test.py
+++ temp-class-test.py
@@ -1,8 +1,14 @@
-from utils.TempSensor import TempSensor
-import json2html 
+#from utils.TempSensor import TempSensor
+from json2html import *
 
 print("starting test tempSensor class..........")
-sensor = TempSensor()
-print(sensor.getAllTemp())
-del sensor
+#sensor = TempSensor()
+#print(sensor.getAllTemp())
+#del sensor
 print("ending test tempSensor class..........")
+
+infoFromJson = {
+        "name": "json2html",
+        "description": "Converts JSON to HTML tabular representation"
+}
+print (json2html.convert(json = infoFromJson))
