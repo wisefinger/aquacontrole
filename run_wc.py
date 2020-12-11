c07290b65dff8bffe89ddc9aedb314346f3b90b3
Kris Cardoen <kris.cardoen@porphyrio.com>
2020-12-11 16:30:56 +0100

    test

---
+++ run_wc.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+print(f'> {datetime.now()} : start run watercycle process................')
+
+
+print(f'> {datetime.now()} : start run watercycle process................')
